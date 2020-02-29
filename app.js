bf2c0f563911e1989211369e0579929effb9db56
joaquincasas <joaquin.casas.albertos@gmail.com>
2020-02-29 19:13:51 +0100

    Creación server Node con express. Conectado server al puerto 8030.

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 8030;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log('started and listening on port ' + port));
+
+app.get('/', (req, res) => {
+  res.send('connected to ' + port);
+});
+
+module.exports = app;
